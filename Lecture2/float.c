7d96151da35363bee834eeb894f4eb9a731945c7
BudacaB <budaca.b@gmail.com>
2020-02-17 23:56:33 +0200

    float

---
+++ Lecture2/float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float price;
+    printf("What is the price?\n");
+    scanf("%f", &price);
+    //printf("Your total is %f.\n", price * 1.0625);
+    printf("Your total is %.2f.\n", price * 1.0625);
+}
